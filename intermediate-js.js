314f6bf185dcf8398e4e5672e81a8e6963168fb7
Ubuntu <ubuntu@ip-172-31-33-188.us-east-2.compute.internal>
2019-06-21 22:42:51 +0000

    finished js file

--- intermediate-js.js
+++ intermediate-js.js
@@ -53,19 +53,39 @@ console.log(countLetter(ourString, 'l'))
 
 // 4. Write a function called getFib that returns the first 10 numbers of the Fibonacci sequence in an array. [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+getFib = (num) => {
+	var newFib = [0,1]
+	for(let i = 2; i<11; i++){
+		newFib[i] = newFib[i -2] + newFib[i -1]
+	}
+	return newFib
+}
+console.log(getFib(10))
+//Done
 
 //5. Write a function called oddChecker that takes in the fullArr and uses a for loop to return a new array of only odd numbers. Copy and paste your code and refactor using ES6 syntax.
 
 var fullArr = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 
-function oddChecker(arr){
+// function oddChecker(arr){
+// 	let newArr = []
+// 	for(let i=0;i<arr.length;i++){
+// 		if(arr[i]%2 !== 0 && typeof arr[i] === "number"){
+// 			newArr.push(i)
+// 		}
+// 	}
+// 	return newArr
+// }
+// console.log(oddChecker(fullArr))
+
+oddChecker = (arr) => {
 	let newArr = []
 	for(let i=0;i<arr.length;i++){
 		if(arr[i]%2 !== 0 && typeof arr[i] === "number"){
-			newArr.push(i)
+		newArr.push(i)
 		}
 	}
 	return newArr
-}
+  }
 console.log(oddChecker(fullArr))
 //Done
